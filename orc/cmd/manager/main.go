7b460f6c315454c237934e266e6215c3ea1dabdd
Matthew Booth <mbooth@redhat.com>
2024-11-18 11:52:42 +0000

    Fix lint error in manager/main.go

--- orc/cmd/manager/main.go
+++ orc/cmd/manager/main.go
@@ -71,7 +71,9 @@ func main() {
 		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
-	flag.IntVar(&scopeCacheMaxSize, "scope-cache-max-size", 10, "The maximum credentials count the operator should keep in cache. Setting this value to 0 means no cache.")
+	flag.IntVar(&scopeCacheMaxSize, "scope-cache-max-size", 10,
+		"The maximum credentials count the operator should keep in cache. "+
+			"Setting this value to 0 means no cache.")
 	opts := zap.Options{
 		Development: true,
 	}
